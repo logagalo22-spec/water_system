d997900ce1c2ed094877ea6929ba5946e28653bb
logagalo22-spec <logagalo22@gmail.com>
2026-03-04 03:40:53 +0800

    Fix mixed content issue by forcing HTTPS scheme in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+
+        if (app()->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 
     /**
